e70aecff9f68f128b4adc92fa55e674277db6adf
jkrusina <josef.krusina@lifeliqe.com>
2024-11-05 15:44:39 +0100

    fix(joi): change import so that the app takes in env correctly

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
 
 @Module({
   // TODO: add other modules that you will create
